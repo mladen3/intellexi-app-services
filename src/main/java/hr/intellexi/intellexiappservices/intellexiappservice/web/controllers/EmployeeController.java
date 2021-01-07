e247343e1b767542ae83b2bd0a5559299af582a3
kguldan <krunoslav.guldan@intellexi.hr>
2021-01-07 15:08:58 +0100

    added EmployeeController, closes #4

---
+++ src/main/java/hr/intellexi/intellexiappservices/intellexiappservice/web/controllers/EmployeeController.java
@@ -0,0 +1,48 @@
+package hr.intellexi.intellexiappservices.intellexiappservice.web.controllers;
+
+import hr.intellexi.intellexiappservices.intellexiappservice.web.model.EmployeeDto;
+import hr.intellexi.intellexiappservices.intellexiappservice.web.services.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/v1/employee")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<EmployeeDto> getEmployee(@PathVariable("employeeId") long employeeId) {
+        return new ResponseEntity<>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> saveBeer(@RequestBody EmployeeDto beerDto) {
+        return new ResponseEntity<>(employeeService.saveEmployee(beerDto), HttpStatus.OK);
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void updateEmployee(@RequestBody EmployeeDto employeeDto) {
+        employeeService.updateEmployee(employeeDto);
+    }
+
+    @DeleteMapping("/{employeeId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteEmployee(@PathVariable("employeeId") long employeeId) {
+        employeeService.deleteById(employeeId);
+    }
+}
